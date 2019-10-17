b99fde122cfc0c05d6e3796da45454102a2e6582
bastienwcs <email>
2019-10-17 09:45:48 +0200

    init project

---
+++ src/main/java/com/wildcodeschool/school/entities/Student.java
@@ -0,0 +1,48 @@
+package com.wildcodeschool.school.entities;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(Long id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/wildcodeschool/school/repositories/StudentRepository.java
@@ -0,0 +1,58 @@
+package com.wildcodeschool.school.repositories;
+
+import com.wildcodeschool.school.entities.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository {
+
+    private static List<Student> students = new ArrayList<Student>() {{
+        add(new Student(0L, "Eric", "Cartman", 11));
+        add(new Student(0L, "Stan", "Marsh", 11));
+        add(new Student(0L, "Kyle", "Broflovski", 11));
+    }};
+
+    public List<Student> findAll() {
+        return students;
+    }
+
+    public Student findById(Long id) {
+        for (Student s : students) {
+            if (s.getId().equals(id)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public Student save(Student student) {
+        for (Student s : students) {
+            if (s.getId().equals(student.getId())) {
+                s.setFirstName(student.getFirstName());
+                s.setLastName(student.getLastName());
+                s.setAge(student.getAge());
+                return student;
+            }
+        }
+        students.add(student);
+        return student;
+    }
+
+    public void	deleteById(Long id) {
+        Student delete = null;
+        for (Student s : students) {
+            if (s.getId().equals(id)) {
+                delete = s;
+                break;
+            }
+        }
+        if (delete != null) {
+            students.remove(delete);
+        }
+    }
+
+    public long count() {
+        return students.size();
+    }
+}
