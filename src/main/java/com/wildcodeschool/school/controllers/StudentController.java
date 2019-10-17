1ba573ab08f569e06f4eb380e834a6cff50d2676
bastienwcs <email>
2019-10-17 10:59:51 +0200

    fix

---
+++ src/main/java/com/wildcodeschool/school/controllers/StudentController.java
@@ -0,0 +1,15 @@
+package com.wildcodeschool.school.controllers;
+
+import com.wildcodeschool.school.repositories.StudentRepository;
+
+public class StudentController {
+
+    // use this to load infos from the database
+    private StudentRepository repository = new StudentRepository();
+
+    // TODO : create a route "/" to the home page
+
+    // TODO : create a route "/list" to the student list
+
+    // TODO : create a route "student?id=$id" to one student infos
+}
